a87bc9fb0c5a36f0cba9b12bd8ab755065b22936
Shirley Shu <130460201+Shirleysyd@users.noreply.github.com>
2026-03-17 10:25:37 +1100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
+  base: '/Garden-sharing-APP/', 
+  plugins: [react(), tailwindcss()],
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
